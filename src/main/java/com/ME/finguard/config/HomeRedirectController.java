bbb839300508aeffa16ebae82e6df20dba36efe1
popka13221 <denis3849484@gmail.com>
2025-12-11 15:19:32 +0300

    Fixed buggs

---
+++ src/main/java/com/ME/finguard/config/HomeRedirectController.java
@@ -0,0 +1,13 @@
+package com.yourname.finguard.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeRedirectController {
+
+    @GetMapping({"/", "/index", "/app", "/app/"})
+    public String rootToLogin() {
+        return "redirect:/app/login.html";
+    }
+}
